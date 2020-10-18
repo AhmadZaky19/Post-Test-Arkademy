e4b226e3cec7649da1f5ab3d4910a3993112e486
AhmadZaky19 <a.zaky32@gmail.com>
2020-10-18 12:20:57 +0700

    Add Logic's Answer No. 6

---
+++ 6.js
@@ -0,0 +1,13 @@
+fibonacci = (numMax) => {
+  for (
+    var fibonacciArray = [], i = 1, j = 0, k = 0;
+    k < numMax;
+    i = j, j = x, k++
+  ) {
+    x = i + j;
+    fibonacciArray.push(x);
+  }
+  console.log(fibonacciArray);
+};
+
+fibonacci(11);
